b497b147367afaaa5f7302c3e43f5081ee52a5f0
Tyuusaku777 <mikagamit777@gmail.com>
2019-04-30 06:54:00 +0900

    Add user_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  #activestorage
+  has_one_attached :image
+
   validates :name, presence: true, length: { maximum: 10}
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 8}
