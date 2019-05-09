3f59187c1c477ab88179f2c5dcc7896f4db97e1f
Tyuusaku777 <mikagamit777@gmail.com>
2019-05-09 16:47:05 +0900

    AddGravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
 
-  #activestorage
-  has_one_attached :image
+  has_secure_password
+
+  has_many :posts
 
   validates :name, presence: true, length: { maximum: 10}
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 8}
 
-  has_secure_password
 
-  has_many :posts
 
 
 
--- db/migrate/20190502044158_create_links.rb
+++
@@ -1,4 +0,0 @@
-class CreateLinks < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
