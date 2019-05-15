9ca8f5796a355ab5b2b245ca3781e0e4c9bc5b61
Tyuusaku777 <mikagamit777@gmail.com>
2019-05-15 16:41:23 +0900

    AlterLikesController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,14 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user=User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||=User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  #いいねの真偽値を返す
   def current_user?
-    !!current_user
+    @current_user ||=User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+
 
   def login_required
     unless current_user
